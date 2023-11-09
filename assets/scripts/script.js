28e2154b2e038c6ee10c7c13f097d721f8776b68
Junx27 <junxitsumo@gmail.com>
2023-11-09 14:26:31 +0700

    navbar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,7 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("nav");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("nav");
+  header.classList.toggle("hidden", window.scrollY > 300);
+});
